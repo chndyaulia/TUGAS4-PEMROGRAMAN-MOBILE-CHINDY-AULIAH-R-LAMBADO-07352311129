a2d08eefa2586aea10058116b3bdc06732567d39
chndyaulia <chndyyauliaa@gmail.com>
2025-10-07 00:56:39 +0700

    Initial commit - tugas 4 mobile

---
+++ tugas4.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+class Mahasiswa {
+  String nama;
+  String npm;
+  String kelas;
+
+  Mahasiswa(this.nama, this.npm, this.kelas);
+
+  void tampilkanData() {
+    print("=== Data Mahasiswa ===");
+    print("Nama  : $nama");
+    print("NPM   : $npm");
+    print("Kelas : $kelas\n");
+  }
+}
+
+// Class Dosen
+class Dosen {
+  String nama;
+  String nip;
+  String matkul;
+
+  Dosen(this.nama, this.nip, this.matkul);
+
+  void tampilkanData() {
+    print("=== Data Dosen ===");
+    print("Nama   : $nama");
+    print("NIP    : $nip");
+    print("Matkul : $matkul\n");
+  }
+}
+
+void main() {
+  // Input data mahasiswa
+  stdout.write("Masukkan nama mahasiswa: ");
+  String? namaMhs = stdin.readLineSync();
+
+  stdout.write("Masukkan NPM mahasiswa: ");
+  String? nim = stdin.readLineSync();
+
+  stdout.write("Masukkan kelas mahasiswa: ");
+  String? kelas = stdin.readLineSync();
+
+  Mahasiswa mhs = Mahasiswa(namaMhs!, nim!, kelas!);
+
+  // Input data dosen
+  stdout.write("\nMasukkan nama dosen: ");
+  String? namaDosen = stdin.readLineSync();
+
+  stdout.write("Masukkan NIP dosen: ");
+  String? nip = stdin.readLineSync();
+
+  stdout.write("Masukkan mata kuliah: ");
+  String? matkul = stdin.readLineSync();
+
+  Dosen dosen = Dosen(namaDosen!, nip!, matkul!);
+
+  // Tampilkan data
+  print("\n==============================");
+  mhs.tampilkanData();
+  dosen.tampilkanData();
+  print("==============================");
+}
